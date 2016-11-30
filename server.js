293bbef6ad37f6d81134cb0d5cebe1a779bd998f
ChaniceStl <chanicestlouis2020@gmail.com>
2016-11-29 23:39:00 -0500

    solve questions 1-18

--- server.js
+++ server.js
@@ -19,6 +19,186 @@ app.get('/view/all-artists', (req, res) => {res.sendFile(path.join(__dirname, '/
 app.get('/view/artists-search', (req, res) => {res.sendFile(path.join(__dirname, '/views/artists-search.html'))});
 app.get('/view/youtube-search', (req, res) => {res.sendFile(path.join(__dirname, '/views/youtube-search.html'))});
 
-//////////
-// YOUR CODE HERE:
-//////////
+app.get('/api/songs', (req,res) => {
+  Song.findAll({
+    include : [Artist]
+  })
+    .then((data) =>{
+      console.log(data);
+      res.send(data);
+    })
+})
+
+// app.get('/api/songs/id/:id', (req, res) => {
+//   Song.findById(req.params.id)
+//     .then((data) => {
+//       console.log(data);
+//       res.send(data);
+//     })
+// })
+
+app.get('/api/songs/id/:id', (req, res) => {
+  Song.findAll({
+    include : [Artist],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+  })
+})
+
+app.get('/api/songs/name/:name', (req, res) => {
+  Song.findAll({
+    include: [Artist],
+    where:{
+      title: req.params.name
+    }
+  })
+    .then((data) => {
+      res.send(data);
+    })
+})
+
+//array only apply to findOne?
+app.get('/api/songs/sort/by-date', (req, res) => {
+  Song.findAll({
+    include : [Artist],
+    order: '"createdAt" DESC'
+  })
+    .then((data) => {
+      res.send(data);
+    })
+})
+
+app.get('/api/songs/sort/a-z', (req,res) => {
+  Song.findAll({
+    include : [Artist],
+    order: "title"
+  })
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+app.get('/api/songs/count' , (req, res) => {
+  Song.count({
+    include: [Artist]
+  })
+    .then((data) =>{
+      console.log(data);
+    })
+})
+
+//yes or no
+app.get('/api/songs/first-five' , (req, res) => {
+    Song.findAll({
+      include: [Artist],
+      order:['createdAt'],
+      limit:5})
+      .then((data) => {
+        res.send(data);
+      })
+  })
+
+app.get('/api/artists',(req,res)=> {
+  Artist.findAll()
+    .then((data) => {
+      res.send(data);
+    })
+})
+
+app.get('/api/artists/sort/a-z', (req,res) => {
+  Artist.findAll({
+    order: 'name'
+  })
+    .then((data) => {
+      res.send(data);
+    })
+})
+
+app.get('/api/artists/id/:id' , (req,res) => {
+  Artist.findById(req.params.id)
+    .then((data) => {
+      res.send(data);
+    })
+})
+
+app.get('/api/artists/name/:name' , (req,res) => {
+  Artist.findOne({
+    where: {
+      name: req.params.name
+    }
+  })
+    .then((data)=> {
+      res.send(data);
+    })
+})
+
+app.get('/api/artists/no-jungle' , (req,res)=>{
+  Artist.findAll({
+    where: {
+      name: {
+        $not : ['Jungle']
+        }
+      }
+    })
+    .then((data) =>{
+      res.send(data);
+    })
+})
+
+app.get('/api/songs-with-artists', (req, res) => {
+  Song.findAll({
+    include: [Artist]
+  })
+    .then((data)=>{
+      res.send(data);
+    })
+})
+
+app.get('/api/artists/frank-or-chromeo', (req, res) => {
+  Song.findAll({
+    include: [{
+      model: Artist,
+      where: {
+        $or: [
+          {name: 'Frank Ocean'},
+          {name: 'Chromeo'}
+        ]
+      }
+    }]
+  })
+  .then((data) => {
+    res.send(data)
+  })
+})
+
+app.post('/api/artists', (req,res) => {
+  Artist.create({name:req.body.name})
+    .then((data)=> {
+        res.send(data);
+    })
+})
+
+app.delete('/api/artists/:id', (req,res) => {
+  Artist.findById(req.params.id)
+    .then(artist => {
+      artist.destroy();
+    })
+    .then(data => {
+        res.send(data);
+    })
+})
+
+app.put('/api/artists/:id', (req,res) =>{
+  Artist.findById(req.params.id)
+  .then(artist => {
+  artist.update({name: "Chanice"})
+  })
+  .then(data => {
+    res.send(data);
+  })
+})
